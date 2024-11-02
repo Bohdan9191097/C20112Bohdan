db511b0261b55f89887b53933f8f00bca6149a4a
simonov328 <simonov328@rambler.ru>
2024-11-02 19:00:58 +0200

    Lesson 1-2

--- Lesson2/student.py
+++ Lesson2/student.py
@@ -3,3 +3,7 @@ class Student:
         self.Name = name
         self.Age = age
         self.Gender = gender
+    def earnMoney(self):
+        for i in range(365):
+            if(i > 150 and i < 240):
+                print(i)
