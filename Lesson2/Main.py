57c76aad90ff881cf32dfb12bb862871a85b0dce
simonov328 <simonov328@rambler.ru>
2024-11-02 18:56:28 +0200

    Lesson 1-2

--- Lesson2/Main.py
+++ Lesson2/Main.py
@@ -1,1 +1,4 @@
-print("Hello world")
+from student import Student
+student = Student("Ivan", age =  15.4, gender = True)
+defaultStudent = Student()
+print(f"Name: {student.Name}\nAge: {student.Age}\nGender: {student.Gender}")
---
+++ Lesson2/student.py
@@ -0,0 +1,5 @@
+class Student:
+    def  __init__(self, name:str = "Test", age: float = 0.0, gender: bool = False):
+        self.Name = name
+        self.Age = age
+        self.Gender = gender
